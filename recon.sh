4f8565b0d52bfda4e9aba6a75b96fd7915b5f54f
Jaza K <jazak@iMac.local>
2020-07-13 00:53:10 -0600

    fix layout/add ipcalc functionality

--- recon.sh
+++ recon.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# reconnaissance script to gather information on a given IP address
+# network reconnaissance script to gather information on a given IP address
 
-RED='\033[0;91m'
-GREEN='\033[0;92m'
-YELLOW='\033[0;93m'
+RED=`tput setaf 196`
+GREEN=`tput setaf 34`
+YELLOW=`tput setaf 3`
 NOCOLOR='\033[0m'
 
 # check if nmap is installed
@@ -12,15 +12,21 @@ if [[ ! -x $NMAP ]]; then
 	exit 1
 fi
 
+# check if ipcalc is installed
+if ! command -v "ipcalc" > /dev/null; then
+	echo "This script requires Ipcalc"
+	exit 1
+fi
+
+# check for proper privileges
 if [[ $EUID -ne 0 ]]; then
 	echo "Run as root"
 	exit 1
 fi
 
-# if no arguments passed, display usage info
-if [ -z "$1" ]
-then
-	echo "Usage: ./recon.sh <target IP address>"
+# if illegal/no arguments passed, display usage info
+if [ -z "$1" ] || [[ $# -ne 2 ]]; then
+	echo "Usage: ./recon.sh <target IP address> <your IP address>"
 	exit 1
 fi
 
@@ -40,7 +46,7 @@ echo -e "${GREEN}Running Nmap scan on target...${NOCOLOR}"
 
 printf "Open ports found on target machine: \n\n" >> results
 # run nmap w/ the ip address provided and send results to file
-nmap -p- -Pn $1 | tail -n +4 | head -n -1 >> results
+nmap -p- -sV -Pn $1 | tail -n +4 | head -n -4 >> results
 
 # do while loop to iterate thru results file
 while read line
@@ -79,13 +85,25 @@ echo -e "${GREEN}Fingerprinting OS...${NOCOLOR}"
 # enable OS detection w/ nmap
 nmap -O -p- $1 | tail -n +9 | head -n -4 >> results
 
+echo -e "${GREEN}Calculating IP network information...${NOCOLOR}"
+
+# code block to determine subnet range
+network=$(ipcalc $1 | tail -n +5 | head -n -5)
+IFS=' '
+array=( $network )
+SUBNET=${array[1]}
+
+printf "\n${YELLOW}------ THIS NETWORK ------${NOCOLOR}\n\n" >> results
+# use ipcalc & target ip address to find network information
+ipcalc $1 >> results
+
 printf "${YELLOW}------ MAC/IP ADDRESSES ------${NOCOLOR}" >> results
 
 echo -e "${GREEN}Retrieving discovered MAC/IP addresses on network...${NOCOLOR}"
 
 printf "Hosts found on network: \n\n" >> results
-# scan network for live hosts & their ip/mac addresses
-nmap -sn 192.168.1.0/24 | tail -n + 3 >> results
+# scan subnet range for live hosts & their mac/ip addresses
+nmap -sn ${SUBNET} | tail -n + 3 >> results
 
 printf "\n" >> results
 cat results
